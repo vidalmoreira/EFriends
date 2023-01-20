b4c4d20bb85310debf3d06d37771d230d3d9d861
Vidal Moreira <moreiravidal22@gmail.com>
2023-01-20 15:53:32 -0300

    Inicio do site

---
+++ HomePage/src/main/java/com/EFriends/EFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.EFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFriendsApplication.class, args);
+	}
+
+}
---
+++ HomePage/src/test/java/com/EFriends/EFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
